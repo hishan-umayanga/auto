0b26caaf5bb69c6324748f5b3016b5594591c0b9
Hishan_uyg <126471878+hishan-umayanga@users.noreply.github.com>
2023-10-19 20:53:31 +0530

    Update settings.py

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'devprojectssl@gmail.com'
-EMAIL_HOST_PASSWORD = "adtmpzjmbknsogre"
+EMAIL_HOST_USER = 'your gmail address'
+EMAIL_HOST_PASSWORD = "your gmail address password"
 
 
 
